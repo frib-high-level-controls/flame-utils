798cd820025fbafc33617cbed5e943404f33fda8
Tong Zhang <zhangt@frib.msu.edu>
2017-01-12 18:08:53 -0500

    Improve interface

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
-# encoding: UTF-8
-
-__copyright__ = "Copyright (c) 2015, Facility for Rare Isotope Beams"
-
-__author__ = "Dylan Maxwell"
-
-
 from .model import Model
+
+from .flame import ModelFlame
+from .flame import MachineStates
+from .flame import generate_latfile
+from .flame import configure
+from .flame import convert_results
+from .flame import collect_data
+from .flame import propagate
+from .flame import get_all_types
+from .flame import get_all_names
+from .flame import inspect_lattice
+from .flame import get_element
+from .flame import get_index_by_type
+from .flame import get_index_by_name
+from .flame import get_names_by_pattern
+
