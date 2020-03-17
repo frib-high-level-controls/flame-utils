c179f38bb40aeda2c99a72f3fd489b25e127c5a9
Tong Zhang <zhangt@frib.msu.edu>
2020-03-17 13:53:31 -0400

    MNT: Bump up version to 0.3.9.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ installrequires = [
 
 setup(
         name=app_name,
-        version="0.3.5",
+        version="0.3.9",
         description=app_description,
         long_description=app_long_description,
         author=app_author,
