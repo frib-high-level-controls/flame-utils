112160a507e3da30337678afb130ea9d70de8144
Tong Zhang <zhangt@frib.msu.edu>
2021-07-08 14:22:53 -0400

    REF: Fix configure method.

--- flame_utils/core/model.py
+++ flame_utils/core/model.py
@@ -463,15 +463,16 @@ class ModelFlame(object):
         Notes
         -----
         Pass `econf` with a list of dict for applying batch configuring.
+        This method is not meant for reconfigure source.
         """
         m = configure(self._mach_ins, econf)
 
-        # update source as well if the econf is for source element,
-        # i.e. element with index of 0.
-        if econf['properties']['type'] == 'source':
-            # self.machine.propagate(self.bmstate.state, 0, 1)
-            zero_s = self.machine.allocState({})
-            _, self.bmstate = self.run(from_element=0, to_element=0, bmstate=zero_s)
+        ## update source as well if the econf is for source element,
+        ## i.e. element with index of 0.
+        #if econf['properties']['type'] == 'source':
+        #    # self.machine.propagate(self.bmstate.state, 0, 1)
+        #    zero_s = self.machine.allocState({})
+        #    _, self.bmstate = self.run(from_element=0, to_element=0, bmstate=zero_s)
 
     def reconfigure(self, index, properties):
         """Reconfigure FLAME model.
